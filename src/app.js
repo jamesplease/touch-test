fbb16928908d7b398c2ae572face3cbbd714144b
James Please <deeper_halal0y@icloud.com>
2022-09-29 10:38:30 -0400

    touch test

--- src/app.js
+++ src/app.js
@@ -1,73 +1,51 @@
 import classNames from 'classnames';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import './app.css';
 
 export default function App() {
   const [tapped, setTapped] = useState(false);
-  const buttonOneRef = useRef();
-  const buttonTwoRef = useRef();
 
   const timeoutRef = useRef();
   function onTap() {
-    console.log('tapped');
     clearTimeout(timeoutRef.current);
     setTapped(true);
-    // timeoutRef.current = setTimeout(() => {
-    //   setTapped(false);
-    // }, 250);
   }
 
   const [lastPointerEvent, setLastPointerEvent] = useState(null);
-
-  const [buttonOnePosition, setButtonOnePosition] = useState(null);
-
-  useEffect(() => {
-    const oneBoundingClientRect = buttonOneRef.current.getBoundingClientRect();
-    const twoBoundingClientRect = buttonTwoRef.current.getBoundingClientRect();
-
-    setButtonOnePosition({
-      left: oneBoundingClientRect.left,
-      left: oneBoundingClientRect.left,
-    });
-  }, []);
 
   return (
     <div
       className="app"
       onPointerDown={(e) => {
-        console.log('e', e);
         setTapped(false);
 
         let touchHeight = e.height ?? 0;
         let touchWidth = e.width ?? 0;
 
         let visualizationModified = false;
-        if (touchHeight === 0 || touchHeight === 1) {
-          touchHeight = 6;
+        if (touchHeight < 3) {
+          touchHeight = 15;
           visualizationModified = true;
         }
 
-        if (touchWidth === 0 || touchWidth === 1) {
-          touchWidth = 6;
+        if (touchWidth < 3) {
+          touchWidth = 15;
           visualizationModified = true;
         }
-
-        const halfHeight = touchHeight / 2;
-        const halfWidth = touchWidth / 2;
 
         setLastPointerEvent({
-          width: e.height,
-          height: e.width,
+          width: touchWidth,
+          height: touchHeight,
           x: e.clientX,
           y: e.clientY,
           visualizationModified,
         });
       }}>
       <div>
-        <button ref={buttonOneRef} className="btn" onClick={onTap}>
+        <button className="btn" onClick={onTap}>
           Button One
         </button>
-        <button ref={buttonTwoRef} className="btn" onClick={onTap}>
+        <button className="btn" onClick={onTap}>
           Button Two
         </button>
       </div>
